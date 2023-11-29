50a0c38b6faff9b98e823b113049c866678dc0f1
Shelodh <jonathan.vigin@epitech.eu>
2023-11-29 15:39:21 +0100

    add Ex4

--- Part1/People.java
+++ Part1/People.java
@@ -1,11 +1,23 @@
 // for the Peasant class you need to store, a name / life / age / attack
 
 class Peasant {
-        public void close() {
+    Private String name;
+    private int age;
+    protected int attack;
+    protected int life;
+
+    public Peasant(String name, int age) {
+        this.name = name;
+        this.age = age;
+        //when the Peasant class is create write a string like this "A New Peasant as join the village, named : {name}, {age} year old";
+        System.out.println("A New Peasant as join the village, named : " + name + ", " + age + " year old");
+    }
+
+    public void close() {
     }
     //when the Peasant class is create write a string like this "A New Peasant as join the village, named : {name}, {age} year old";
 
-    //when the Peasant class is destroy you need to displau "{name} has leave the village at {age + 5}"
+    //when the Peasant class is destroy you need to display "{name} has leave the village at {age + 5}"
 }
 
 // EX4 ensures that a Soldier class is created; the soldier defaults to a sword as a weapon (15 points of damage and 100 life).
